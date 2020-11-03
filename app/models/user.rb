a78f667c67e093d7937386276955b2eccfda812b
nakahashi-yuki <nakaba19990224@icloud.com>
2020-11-03 16:36:39 +0900

    新規登録、ログインボタンの遷移

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname, presence: true
-         validates :email, presence: true
-         validates :password, presence: true
-         validates :a, presence: true
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :L_name, presence: true
+  validates :F_name, presence: true
+  validates :birthday, presence: true
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,9 +16,11 @@ ActiveRecord::Schema.define(version: 2020_11_02_090753) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "nickname", null: false
-    t.string "fullname", null: false
-    t.string "name", null: false
-    t.integer "birthday", null: false
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "L_name", null: false
+    t.string "F_name", null: false
+    t.date "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
