cf116da1d379b1f468e9542a35bce18d33a042d3
nakahashi-yuki <nakaba19990224@icloud.com>
2020-11-04 12:17:53 +0900

    新規登録、ログイン・ログアウトの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  with_options presence: true do
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :L_name, format: { with: /\A[ァ-ヶー－]+\z/}
+    validates :F_name, format: { with: /\A[ァ-ヶー－]+\z/}
+  end
+  PASSWORD_REGEX = /\A[a-zA-Z0-9]+\z/.freeze
+  validates :password, format: { with: PASSWORD_REGEX }
+  validates :encrypted_password, presence: true
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :L_name, presence: true
-  validates :F_name, presence: true
   validates :birthday, presence: true
+
 
 end
