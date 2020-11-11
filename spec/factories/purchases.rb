17a571620551d0782648eae3fa0386efabe73e09
nakahashi-yuki <nakaba19990224@icloud.com>
2020-11-11 19:49:41 +0900

    テストコード 完了

--- spec/factories/purchases.rb
+++ spec/factories/purchases.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
-  factory :purchase do
-    
+  factory :PurchasesStreetAddress do
+    token {"tok_abcdefghijk00000000000000000"}
+    postal_code { 123-1234}
+    prefectures_id {2}
+    municipality {"福岡市"}
+    address {"中洲"}
+    phone_number {"09033336666"}
   end
 end
