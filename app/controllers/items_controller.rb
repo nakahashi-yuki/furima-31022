8e181cd7e40b8a2d9e39a88295ef50ec8534e309
nakahashi-yuki <nakaba19990224@icloud.com>
2020-11-11 20:55:22 +0900

    Basic認証を導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :basic_auth
   before_action :authenticate_user!, except: [:index, :show]
   before_action :find_params_id, except: [:index, :new, :create]
 
@@ -47,5 +48,11 @@ class ItemsController < ApplicationController
 
   def find_params_id
     @item = Item.find(params[:id])
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 end
